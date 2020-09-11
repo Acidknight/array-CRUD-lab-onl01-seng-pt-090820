c161828dba7b47cf91e3fbec70fb354c5acce48e
Anthony Leon <acidknights89@gmail.com>
2020-09-11 03:35:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  video_game_character[Link, Samus, Mario, Luigi]
+  video_game_character = [Link, Samus, Mario, Luigi]
 end
 
 def add_element_to_end_of_array(array, element)
