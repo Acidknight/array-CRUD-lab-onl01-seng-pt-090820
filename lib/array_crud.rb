b82590ef734dc629184804c63710fb76ef4027e7
Anthony Leon <acidknights89@gmail.com>
2020-09-11 03:44:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   video_game_character = ["Link", "Samus", "Mario", "Luigi"]
-  
+  arrays! = 
 end
 
 def remove_element_from_start_of_array(array)
