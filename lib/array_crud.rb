8f0827034d4d0db2ea7b6837ecc9695ff6f6c341
Anthony Leon <acidknights89@gmail.com>
2020-09-11 03:47:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  video_game_character = ["Link", "Samus", "Mario", "Luigi"]
+  video_game_character = ["Link", "Samus", "Mario", "Luigi", "arrays!"]
   luigi = video_game_character.pop
 end
 
