0f121425c9a37b6c16738c34f698f289f9362629
Anthony Leon <acidknights89@gmail.com>
2020-09-11 03:52:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  video_game_character = ["Link", "Samus", "Mario", "Luigi"]
+  video_game_character = ["wow", "Samus", "Mario", "Luigi"]
   video_game_character[0]
 end
 
