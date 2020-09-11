1c6f3090b5c4f3ef906d3d89eddbe2206bc262af
Anthony Leon <acidknights89@gmail.com>
2020-09-11 03:40:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   #video_game_character = ["Link", "Samus", "Mario", "Luigi"]
-  video_game_character << "Princess Peach"
+  video_game_character << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
